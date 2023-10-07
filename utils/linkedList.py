dbfd5895dd23ffdaefd848726a338867f48792da
VLBortolotti <vitorlacobortolotti>
2023-10-07 14:03:36 -0300

    Update: linkedList methods

--- utils/linkedList.py
+++ utils/linkedList.py
@@ -5,14 +5,14 @@ class Node:
 
 class LinkedList:
     def __init__(self):
-        self.inicio = None
-        self.tamanho = 0
+        self.start = None
+        self.size = 0
 
     def empty(self):
-        return self.inicio == None
+        return self.start == None
     
     def printList(self):
-        aux = self.inicio
+        aux = self.start
         
         while (aux != None):
             print(f"value: {aux.info}")
@@ -21,12 +21,12 @@ class LinkedList:
     def deleteNode(self, position):
         if (not self.empty() and position > 0):
             if (position == 1):
-                self.inicio = self.inicio.next
-                self.tamanho -= 1
+                self.start = self.start.next
+                self.size -= 1
 
             else:
                 i = 1
-                aux = self.inicio
+                aux = self.start
                 ant = None
                 while(i < position and aux != None):
                     ant = aux
@@ -35,19 +35,19 @@ class LinkedList:
                 
                 if (aux != None):
                     ant.next = aux.next
-                    self.tamanho -= 1
+                    self.size -= 1
     
     def insert(self, position, value):
         if (position > 0):
             novo = Node(value)
             
             if (position == 1):
-                novo.next = self.inicio
-                self.inicio = novo
-                self.tamanho += 1
+                novo.next = self.start
+                self.start = novo
+                self.size += 1
             
             else:
-                aux = self.inicio
+                aux = self.start
                 i = 1
                 
                 while (i < position - 1 and aux != None):
@@ -57,22 +57,25 @@ class LinkedList:
                 if (aux != None):
                     novo.next = aux.next
                     aux.next = novo
-                    self.tamanho += 1
+                    self.size += 1
 
-    def position(self, value):
-        aux = self.inicio
+    def getPositionByValue(self, value):
+        aux = self.start
         i = 1
+
         while (aux != None):
             if (aux.info == value):
                 return i
             aux = aux.next
             i += 1
+
         return 0
     
-    def value(self, position):
+    def getValueByPosition(self, position):
         if (position > 0):
-            aux = self.inicio
+            aux = self.start
             i = 1
+            
             while (i < position and aux != None):
                 aux = aux.next
                 i += 1
@@ -83,13 +86,13 @@ class LinkedList:
         return -1
 
     def getSize(self):
-        return self.tamanho
+        return self.size
         
-    def compareLists(self, lista2):
-        aux1 = self.inicio
-        aux2 = lista2.inicio
+    def compareLists(self, list2):
+        aux1 = self.start
+        aux2 = list2.start
 
-        if (self.getSize() == lista2.getSize()):
+        if (self.getSize() == list2.getSize()):
 
             while (aux1 != None and aux2 != None):
                 if (aux1.info != aux2.info):
@@ -103,41 +106,40 @@ class LinkedList:
         return False
 
     def reverseList(self):
-        aux = self.inicio
-        listaContigua = []
+        aux = self.start
+        contiguousList = []
 
         while (aux != None):
-            listaContigua.append(aux.info)
+            contiguousList.append(aux.info)
             aux = aux.next
 
-        novaLista = LinkedList()
+        newList = LinkedList()
         
         cont = 1
-        for i in range(len(listaContigua)-1, -1, -1):
-            novaLista.insert(cont, listaContigua[i])
+        for i in range(len(contiguousList)-1, -1, -1):
+            newList.insert(cont, contiguousList[i])
             cont += 1
 
-        return novaLista
+        return newList
         
-    def destroy(self):
-        self.inicio = None
+    def destroyList(self):
+        self.start = None
 
+# lista = LinkedList()
+# lista.insert(1, '1')
 
-lista = LinkedList()
-lista.insert(1, '1')
+# lista.insert(2, '2')
 
-lista.insert(2, '2')
+# list2 = LinkedList()
+# list2.insert(1, '1')
+# list2.insert(2, '2')
 
-lista2 = LinkedList()
-lista2.insert(1, '1')
-lista2.insert(2, '2')
+# print("lista")
+# list2.printList()
 
-print("lista")
-lista2.printList()
+# print("lista invertida")
+# listaInvertida = list2.reverseList()
+# listaInvertida.printList()
 
-print("lista invertida")
-listaInvertida = lista2.reverseList()
-listaInvertida.printList()
-
-print(lista.getSize())
-print(lista.position(2))
+# print(lista.getSize())
+# print(lista.getPositionByValue(2))
